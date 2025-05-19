7ae46498c9ac104d2e588247a72ecd16216452f0
Lokesh Rana <lokeshrana9999@gmail.com>
2025-05-19 22:54:30 +0530

    feat: db setup udpated to sqlite

--- config/database.ts
+++ config/database.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 export default ({ env }) => {
 
+
   if (env('DATABASE_CLIENT') === 'sqlite') {
     return {
       connection: {
@@ -13,15 +14,8 @@ export default ({ env }) => {
 
   return {
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', 'database-1.cjl5dn4w6t2w.ap-south-1.rds.amazonaws.com'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'postgres'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'ultimate'),
-        ssl: { rejectUnauthorized: false }
-      },
+      filename: path.join(__dirname, '..', '..', '.db/current.db'),
     },
+    useNullAsDefault: true,
   };
 };
