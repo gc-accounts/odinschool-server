ab3664506a552ee12269287b9e773c37ca056bb7
Lokesh Rana <lokeshrana9999@gmail.com>
2025-05-25 23:51:38 +0530

    fix: is feature and is odintalk by default false

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1048,8 +1048,8 @@ export interface ApiWebinarWebinar extends Struct.CollectionTypeSchema {
     duration: Schema.Attribute.Integer;
     image_url: Schema.Attribute.String;
     Instructor: Schema.Attribute.String;
-    is_featured: Schema.Attribute.Boolean;
-    is_odin_talk: Schema.Attribute.Boolean;
+    is_featured: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    is_odin_talk: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
