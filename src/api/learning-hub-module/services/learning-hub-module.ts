cdd6f1118fb3e72da36ab9204b185b568d695a75
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-06-19 16:52:19 +0530

    lb

---
+++ src/api/learning-hub-module/services/learning-hub-module.ts
@@ -0,0 +1,7 @@
+/**
+ * learning-hub-module service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::learning-hub-module.learning-hub-module');
