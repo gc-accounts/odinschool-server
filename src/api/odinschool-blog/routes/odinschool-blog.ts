1b12ac6b2a83547c2b7fd2ddad92a2cd03eaf67a
gc-accounts <jatin@odinschool.com>
2025-06-19 13:23:55 +0530

    odinschool blog collection created

---
+++ src/api/odinschool-blog/routes/odinschool-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * odinschool-blog router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::odinschool-blog.odinschool-blog');
---
+++ src/api/odinschool-blog/services/odinschool-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * odinschool-blog service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::odinschool-blog.odinschool-blog');
